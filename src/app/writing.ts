35fb9d96894adee5ce83e6a5631be4cdd21beac5
Sean Trane <seantrane@users.noreply.github.com>
2018-10-24 13:16:38 -0400

    fix: log message at start of file-writing task

--- src/app/writing.ts
+++ src/app/writing.ts
@@ -1,4 +1,7 @@
+import chalk from 'chalk';
+
 export async function writing(yo) {
+  yo.log('\n\n\n' + chalk.bold.cyanBright('Writing files...'));
   yo.fs.copyTpl(yo.templatePath('.editorconfig'), yo.destinationPath('.editorconfig'));
 }
 
