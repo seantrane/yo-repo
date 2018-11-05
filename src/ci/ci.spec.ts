0b114c14b12021ac9bfcfaa48110d6d690648f7f
Sean Trane <seantrane@users.noreply.github.com>
2018-11-05 05:10:19 -0500

    test: add unit tests for 'yo repo:ci'

---
+++ src/ci/ci.spec.ts
@@ -0,0 +1,49 @@
+import { expect, should } from 'chai';
+import * as mocha from 'mocha';
+import * as path from 'path';
+import * as assert from 'yeoman-assert';
+import * as helpers from 'yeoman-test';
+
+import { RepoCiGenerator } from './';
+
+describe.skip('yo repo ci', function() {
+
+  describe('RepoCiGenerator class', function() {
+
+    it('should have expected methods', function() {
+      expect(RepoCiGenerator).to.respondTo('initializing');
+      expect(RepoCiGenerator).to.respondTo('prompting');
+      expect(RepoCiGenerator).to.respondTo('configuring');
+      expect(RepoCiGenerator).to.respondTo('writing');
+    });
+
+  });
+
+  describe('the generator', function() {
+
+    it('should generate .travis.yml file with expected contents', function(done) {
+      setTimeout(done, 300);
+      helpers.run(RepoCiGenerator)
+        .withPrompts({ cicd: 'travis' })
+        .then(function(dir) {
+          assert.file('.travis.yml');
+          // assert.fileContent('.travis.yml', /.*/);
+          done();
+        })
+        .catch(done);
+    });
+
+    it('should generate .travis.yml file by default, without prompt', function(done) {
+      setTimeout(done, 300);
+      helpers.run(RepoCiGenerator)
+        .then(function(dir) {
+          assert.file('.travis.yml');
+          // assert.fileContent('.travis.yml', /.*/);
+          done();
+        })
+        .catch(done);
+    });
+
+  });
+
+});
