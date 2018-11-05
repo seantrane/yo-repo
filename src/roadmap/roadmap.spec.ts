346f1b9459889191c0b48b2d79be817592735443
Sean Trane <seantrane@users.noreply.github.com>
2018-11-05 05:11:31 -0500

    test: add unit tests for 'yo repo:roadmap'

---
+++ src/roadmap/roadmap.spec.ts
@@ -0,0 +1,49 @@
+import { expect, should } from 'chai';
+import * as mocha from 'mocha';
+import * as path from 'path';
+import * as assert from 'yeoman-assert';
+import * as helpers from 'yeoman-test';
+
+import { RepoRoadmapGenerator } from './';
+
+const appSpec = require('../app/app.spec.json');
+
+describe.skip('yo repo roadmap', function() {
+
+  describe('RepoRoadmapGenerator class', function() {
+
+    it('should have expected methods', function() {
+      expect(RepoRoadmapGenerator).to.respondTo('initializing');
+      expect(RepoRoadmapGenerator).to.respondTo('prompting');
+      expect(RepoRoadmapGenerator).to.respondTo('configuring');
+      expect(RepoRoadmapGenerator).to.respondTo('writing');
+    });
+
+  });
+
+  describe('the generator', function() {
+
+    it('should generate ROADMAP.md file with expected contents', function(done) {
+      setTimeout(done, 300);
+      helpers.run(RepoRoadmapGenerator)
+        .withPrompts(appSpec.answers.default)
+        .then(function(dir) {
+          assert.file('ROADMAP.md');
+          done();
+        })
+        .catch(done);
+    });
+
+    it('should generate ROADMAP.md file by default, without prompts', function(done) {
+      setTimeout(done, 300);
+      helpers.run(RepoRoadmapGenerator)
+        .then(function(dir) {
+          assert.file('ROADMAP.md');
+          done();
+        })
+        .catch(done);
+    });
+
+  });
+
+});
