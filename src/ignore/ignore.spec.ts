0e106745c8c00bb9f63d046809656f3fe6b240e7
Sean Trane <seantrane@users.noreply.github.com>
2018-11-05 05:11:00 -0500

    test: add unit tests for 'yo repo:ignore'

---
+++ src/ignore/ignore.spec.ts
@@ -0,0 +1,38 @@
+import { expect, should } from 'chai';
+import * as mocha from 'mocha';
+import * as path from 'path';
+import * as assert from 'yeoman-assert';
+import * as helpers from 'yeoman-test';
+
+import { RepoIgnoreGenerator } from './';
+
+describe.skip('yo repo ignore', function() {
+
+  describe('RepoIgnoreGenerator class', function() {
+
+    it('should have expected methods', function() {
+      expect(RepoIgnoreGenerator).to.respondTo('initializing');
+      expect(RepoIgnoreGenerator).to.respondTo('prompting');
+      expect(RepoIgnoreGenerator).to.respondTo('configuring');
+      expect(RepoIgnoreGenerator).to.respondTo('writing');
+    });
+
+  });
+
+  describe('the generator', function() {
+
+    it('should generate .gitignore and .npmignore files with expected contents', function(done) {
+      setTimeout(done, 300);
+      helpers.run(RepoIgnoreGenerator)
+        // .withPrompts(appSpec.answers.default)
+        .then(function(dir) {
+          assert.file('.gitignore');
+          assert.file('.npmignore');
+          done();
+        })
+        .catch(done);
+    });
+
+  });
+
+});
